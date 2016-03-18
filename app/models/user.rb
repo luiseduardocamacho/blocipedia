68f4b57b058fd7a13f12d166e6ed52afb1f329a7
Luis Camacho <luiscamacho@gmail.com>
2016-03-18 16:52:39 -0400

    Added Devise for user authentication, added mailcatcher to view emails, added bootstrap, changed defualt devise views

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+         before_save { self.email = email.downcase }
+
+         validates :password, presence: true, length: { minimum: 6 }, if: "password.nil?"
+         validates :password, length: { minimum: 6 }, allow_blank: true
+
+         EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+         validates :email,
+            presence: true,
+            length: { minimum: 8, maximum: 100 },
+            format: { with: EMAIL_REGEX }
+
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+
+
+  it {should validate_presence_of (:email)}
+  it {should validate_uniqueness_of (:email)}
+  it {should validate_length_of(:email).is_at_least(8)}
+
+  it {should allow_value("user@blocipedia.com").for(:email)}
+  it {should_not allow_value("userbloccit.com").for(:email)}
+
+  it {should validate_presence_of(:password)}
+  it {should validate_length_of(:password).is_at_least(8)}
+
+  describe "attributes" do
+    describe User do
+      before :each do
+        @user = FactoryGirl.create(:user)
+      end
+      it "should have email" do
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "invalid user" do
+
+    let(:user_with_invalid_name){User.new(name: "", email: "test@test.com")}
+    let (:user_with_invalid_email){User.new(name:"Blocipedia User", email: "")}
+
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+  end
+end
