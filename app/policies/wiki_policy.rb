2de2be79656c3f16d2e3802fffa6e697fa20a1e2
Luis Camacho <luiscamacho@gmail.com>
2016-04-10 11:27:39 -0400

    Seeded database

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,6 +1,6 @@
 class WikiPolicy < ApplicationPolicy
   def update?
     #user.premium? or user.standard? or record.private == false or record.user == user
-    record.private == false or @record.user == user
+    !record.private or @record.user == user
   end
 end
