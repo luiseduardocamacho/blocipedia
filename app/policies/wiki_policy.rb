ec874c8c2126210a593db4ffe3aaaca6913acf10
Luis Camacho <luiscamacho@gmail.com>
2016-05-01 20:32:19 -0400

    Added redcarpet markdown gem and updated wiki update policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,7 +1,7 @@
 class WikiPolicy < ApplicationPolicy
   def update?
     #user.premium? or user.standard? or record.private == false or record.user == user
-    !record.private or @record.user == user
+    !record.private or !(@user.standard?)
   end
 
   def edit?
